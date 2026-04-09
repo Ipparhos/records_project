3e00cca597de5f749db00c9be3183e1be6668a80
Pantelis <pantelisdio@gmail.com>
2026-04-09 19:34:15 +0300

    solve favicon bug

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("records.urls")),
+    path("favicon.ico", RedirectView.as_view(url="/static/favicon.svg")),
     re_path(r"^.*$", TemplateView.as_view(template_name="index.html")),
 ]
+
